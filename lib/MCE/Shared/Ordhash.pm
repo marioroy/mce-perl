b153cdef1f8fcf37abba94c868612a1fb81d4e47
Mario Roy <marioeroy@gmail.com>
2016-02-21 12:15:19 -0500

    adjustment for tombstone garbage collection

--- lib/MCE/Shared/Ordhash.pm
+++ lib/MCE/Shared/Ordhash.pm
@@ -174,7 +174,7 @@ sub DELETE {
    $keys->[ $id - $self->[_BEGI] ] = _TOMBSTONE;
 
    # GC keys and indx if more than half are tombstone
-   if ( ++$self->[_GCNT] > ( @{ $keys } >> 2 ) * 3 ) {
+   if ( ++$self->[_GCNT] >= ( @{ $keys } >> 2 ) * 3 ) {
       my $i = 0;
       for my $k ( @{ $keys } ) {
          $keys->[ $i ] = $k, $indx->{ $k } = $i++ if ( defined $k );
