b0646d155f8540c701668d263dc319780afda4cd
Mario Roy <marioeroy@gmail.com>
2017-09-02 15:14:08 -0500

    Updates for the upcoming MCE 1.830 release.

--- lib/MCE/Signal.pm
+++ lib/MCE/Signal.pm
@@ -227,7 +227,7 @@ sub stop_and_exit {
             if ($_sig_name eq 'PIPE' && $INC{'MCE/Hobo.pm'}) {
                CORE::kill('QUIT', $_is_MSWin32 ? -$$ : -getpgrp);
             }
-            if ($_sig_name eq 'INT' && -t STDIN) {
+            if ($_sig_name eq 'INT' && -t STDIN) { ## no critic
                print {*STDERR} "\n";
             }
             if ($INC{'threads.pm'} && ($] lt '5.012000' || threads->tid())) {
